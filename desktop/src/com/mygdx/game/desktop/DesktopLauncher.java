25177c6ee9ccc494b9a3e6b25c81047b28208e39
HereDrlv <a549414560@163.com>
2017-11-23 21:00:43 +0800

    调整注释书写

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -25,68 +25,66 @@ public class DesktopLauncher {
 			Ship ship3 = testGameController.getNextPlayer().getShipBatch().get(0);//nextPlayer Submarine
 			Ship ship4 = testGameController.getNextPlayer().getShipBatch().get(1);//nextPlayer SupplyingShip
 		
-		/*
-			//测试打印所有Tile
-			for (Tile t : Tile.tileArray) {
-				System.out.print("tilePosition:"+t.getPositionX()+"  ");
-			}
-			System.out.println();
-		*/
+
+//			//测试打印所有Tile
+//			for (Tile t : Tile.tileArray) {
+//				System.out.print("tilePosition:"+t.getPositionX()+"  ");
+//			}
+//			System.out.println();
+
 		
-		/*
 			//测试打印nowPlayer的所有Ship
 
-			System.out.print("shipType:"+ship1.getClass()+"  ");
-			System.out.print("shipType:"+ship2.getClass()+"  ");
-			System.out.print("shipType:"+ship3.getClass()+"  ");
-			System.out.print("shipType:"+ship4.getClass()+"  ");			
-			System.out.println();
-		 */
+//			System.out.print("shipType:"+ship1.getClass()+"  ");
+//			System.out.print("shipType:"+ship2.getClass()+"  ");
+//			System.out.print("shipType:"+ship3.getClass()+"  ");
+//			System.out.print("shipType:"+ship4.getClass()+"  ");			
+//			System.out.println();
+		 
 		
-		/*
+		
 			//获取可达Tiles,此处以ship1为例
-			ArrayList<Tile> reachabletiles = ship1.showReachableTiles();
-			for(Tile t : reachabletiles) {
-				System.out.println(t.getPositionX());
-			}
-		
+//			ArrayList<Tile> reachabletiles = ship1.showReachableTiles();
+//			for(Tile t : reachabletiles) {
+//				System.out.println(t.getPositionX());
+//			}
+//		
 			//移动至可达Tile
-			Tile goalTile = reachabletiles.get(0);
-			System.out.println("position before moving:" + ship1.getPositionTile().getPositionX()
-			);
-			System.out.println("Now the ship at goalTile is :"+goalTile.getShipAtThisTile());
-			ship1.moveTo(goalTile);//此处以可达Array中的第一个位置为测试用例
-			
-			System.out.println("position after moving:" + ship1.getPositionTile().getPositionX()
-			);
-			System.out.println("Now the ship at goalTile is :"+goalTile.getShipAtThisTile());
+//			Tile goalTile = reachabletiles.get(0);
+//			System.out.println("position before moving:" + ship1.getPositionTile().getPositionX()
+//			);
+//			System.out.println("Now the ship at goalTile is :"+goalTile.getShipAtThisTile());
+//			ship1.moveTo(goalTile);//此处以可达Array中的第一个位置为测试用例
+//			
+//			System.out.println("position after moving:" + ship1.getPositionTile().getPositionX()
+//			);
+//			System.out.println("Now the ship at goalTile is :"+goalTile.getShipAtThisTile());
 		
-		*/
-		/*
+		
+		
 			//(普通船)获取可攻击到的Ships
 			
-			ArrayList<Ship>	attackabletiles = ship2.showAttackableShips();
-			for(Ship s : attackabletiles ) {
-				System.out.println(s.getPositionTile().getPositionX());
-			}
+//			ArrayList<Ship>	attackabletiles = ship2.showAttackableShips();
+//			for(Ship s : attackabletiles ) {
+//				System.out.println(s.getPositionTile().getPositionX());
+//			}
 			//攻击
-			System.out.println("ship2HP:"+ship2.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
-			ship2.attack(attackabletiles.get(0));//以能攻击到的第一条船为例
-			System.out.println("ship2HP:"+ship2.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
+//			System.out.println("ship2HP:"+ship2.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
+//			ship2.attack(attackabletiles.get(0));//以能攻击到的第一条船为例
+//			System.out.println("ship2HP:"+ship2.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
 			//攻击成功。反击成功
-		*/
-		/*
+		
 			//(奶船)获取可攻击到的Ships
-			ArrayList<Ship> attackabletiles = ship4.showAttackableShips();
-			for(Ship s : attackabletiles ) {
-				System.out.println(s.getPositionTile().getPositionX());
-			}
+//			ArrayList<Ship> attackabletiles = ship4.showAttackableShips();
+//			for(Ship s : attackabletiles ) {
+//				System.out.println(s.getPositionTile().getPositionX());
+//			}
 			//攻击
-			System.out.println("ship4HP:"+ship4.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
-			ship4.attack(attackabletiles.get(0));//以能攻击到的第一条船为例
-			System.out.println("ship4HP:"+ship4.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
+//			System.out.println("ship4HP:"+ship4.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
+//			ship4.attack(attackabletiles.get(0));//以能攻击到的第一条船为例
+//			System.out.println("ship4HP:"+ship4.getNowHP()+"enmHp:"+attackabletiles.get(0).getNowHP());
 			//回血成功。不会反击
-		*/	
+			
 
 	}
 }
